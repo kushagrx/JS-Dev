11309feb4cc6131534c23e9a2066b60f623d5173
kushagrx <kushagrabisht10@gmail.com>
2025-09-08 18:33:45 +0530

    created a toggle night mode button using event listeners

--- Events/eventscript.js
+++ Events/eventscript.js
@@ -14,3 +14,25 @@ box.onmouseover = () => {
 box.onmouseout = () => {
     box.style.backgroundColor = "green";
 }
+
+box.addEventListener("click", () => {
+    box.style.backgroundColor = "blue";
+});
+
+
+const toggle = document.querySelector("#btn2");
+let color = "light";
+toggle.addEventListener("click", () => {
+    if(color === "light"){
+        document.body.style.backgroundColor = "#333";
+        document.body.style.color = "#fff";
+        toggle.textContent = "Toggle light mode";
+        color = "dark";
+    }
+    else{
+        document.body.style.backgroundColor = "#fff";
+        document.body.style.color = "#333";
+        toggle.textContent = "Toggle dark mode";
+        color = "light";
+    }
+});
